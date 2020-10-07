ad60426bbcd5d498c79e95cd06bb3ae4e35c0f6e
Emerson Zaro <emerson.tecnologia@supersoft.com.br>
2020-10-07 16:01:01 -0300

    métodos de listagem

--- class/usuario.php
+++ class/usuario.php
@@ -64,6 +64,50 @@ class Usuario {
 
     }
 
+    public static function getList() {
+
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM  tb_usuarios ORDER BY deslogin");
+    }
+
+    public static function search($login) {
+
+        $sql = new Sql;
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+
+    }
+
+    public function login($login, $password) {
+        
+        $sql = new Sql();
+
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD;", array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+
+        if (count($result) > 0) {
+
+            //linha
+            $row = $result[0];
+
+            //setter para alimentar os dados
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+         } else {
+
+             throw new Exception("Login e/ou senha inválidos");
+
+         }
+
+    } 
+
+    
     // transforma dados em string
     // usa o método mágio __toString
     public function __toString() {
@@ -78,6 +122,4 @@ class Usuario {
     }
 
 }
-
-
 ?>
--- index.php
+++ index.php
@@ -7,8 +7,24 @@ $usuarios = $sql->select("SELECT * FROM  tb_usuarios");
 echo json_encode($usuarios);
 **/
 
-$root = new Usuario();
-$root->loadById(2);
-echo $root;
+
+//Carrega um únuco usuário
+//$root = new Usuario();
+//$root->loadById(2);
+//echo $root;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega lista de usuários pelo login
+//$search = Usuario::search("Jo");
+//echo json_encode($search);
+
+//Carrega usuário usando login e senha
+$usuario = new Usuario();
+$usuario->login("Emerson", "1158");
+
+echo $usuario;
 
 ?>
